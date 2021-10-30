7e44421be925694b42bea885acafaf063608a383
Justin Patterson <jhpatt33@gmail.com>
2021-10-29 20:37:30 -0400

    Level Control updated, Generate Level Typo fixed, lighting probably resolved

--- Final Project/Assets/Scripts/Environment/GenerateLevel.cs
+++ Final Project/Assets/Scripts/Environment/GenerateLevel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneralLevel : MonoBehaviour
+public class GenerateLevel : MonoBehaviour
 {
     public GameObject[] section;
     public int zPos = 50;
@@ -20,7 +20,7 @@ public class GeneralLevel : MonoBehaviour
     }
     IEnumerator GenerateSection()
     {
-        secNum = Random.Range(0, 3);
+        secNum = Random.Range(0, section.Length);
         Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
         zPos += 50;
         yield return new WaitForSeconds(2);
